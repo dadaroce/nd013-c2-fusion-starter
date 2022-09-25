163e152ae32ca88d2afa5b035dab59b357e1d990
dadaroce <danielcediel97@gmail.com>
2022-09-25 14:30:28 -0500

    [ERR]: typo errors

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -130,11 +130,11 @@ def bev_from_pcl(lidar_pcl, configs):
     intensity_map = np.zeros((configs.bev_height + 1, configs.bev_width + 1))
     # step 2 : re-arrange elements in pcl_copy by sorting first by x, then y, then -z (use numpy.lexsort)
     idx = np.lexsort((-pcl_copy[:,2], pcl_copy[:,1], pcl_copy[:,0]))
-    pcl_cpy_sort = pcl_copy[idx]
+    lidar_pcl_cpy = pcl_copy[idx]
     ## step 3 : extract all points with identical x and y such that only the top-most z-coordinate is kept (use numpy.unique)
     ##          also, store the number of points per x,y-cell in a variable named "counts" for use in the next task
-    _, idx_unique = np.unique(pcl_cpy_sort[:, 0:2], return_index=True, axis=0)
-    lidar_pcl_top = pcl_cpy_sort[idx_unique]
+    _, idx_unique = np.unique(lidar_pcl_cpy[:, 0:2], return_index=True, axis=0)
+    lidar_pcl_top = lidar_pcl_cpy[idx_unique]
     ## step 4 : assign the intensity value of each unique entry in lidar_top_pcl to the intensity map 
     ##          make sure that the intensity is scaled in such a way that objects of interest (e.g. vehicles) are clearly visible    
     ##          also, make sure that the influence of outliers is mitigated by normalizing intensity on the difference between the max. and min. value within the point cloud
@@ -142,16 +142,15 @@ def bev_from_pcl(lidar_pcl, configs):
     lidar_pcl_top[lidar_pcl_top[:, 3] < 0.0, 3] = configs.lim_r[0]
     intensity_map[np.int_(lidar_pcl_top[:,0]), np.int_(lidar_pcl_top[:,1])] = (lidar_pcl_top[:,3]-min(lidar_pcl_top[:,3]))/float(max(lidar_pcl_top[:,3])-min(lidar_pcl_top[:,3]))
     ## step 5 : temporarily visualize the intensity map using OpenCV to make sure that vehicles separate well from the background
-    img_intensity = intensity_map * 256
-    img_intensity = img_intensity.astype(np.uint8)
-
-    cv2.imshow('img_intensity', img_intensity)
+    # img_intensity = intensity_map * 256
+    # img_intensity = img_intensity.astype(np.uint8)
+    cv2.imshow('img_intensity', intensity_map)
     cv2.waitKey(1)
-    # while (1):
-    #     cv2.imshow('img_intensity', img_intensity)
-    #     if cv2.waitKey(10) & 0xFF == 27:
-    #         break
-    # cv2.destroyAllWindows()
+    while (1):
+        cv2.imshow('img_intensity', intensity_map)
+        if cv2.waitKey(10) & 0xFF == 27:
+            break
+    cv2.destroyAllWindows()
     #######
     ####### ID_S2_EX2 END ####### 
 
